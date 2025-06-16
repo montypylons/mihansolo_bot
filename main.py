8605da815f9352695dfd6224f9863c01f372f282
pi-thon42 <mbenaragama@nastudents.org>
2025-06-16 19:55:12 -0400

    add functions

--- main.py
+++ main.py
@@ -8,14 +8,22 @@ knight = 3
 bishop = 3
 queen = 9
 
+class Board:
+    pass
+def check_forcing_moves(board):
+    pass
 def evaluate_piece(piece):
     pass
 def evaluate_position(board):
     pass
 def reject_moves(board):
     pass
-
-
+def get_best_move(board):
+    pass
+def display_board(board):
+    pass
+def update_board(board, move):
+    pass
 
 
 # TODO: make it work
