69d1a516be54b352cd1a36db30e7a6ca1823d758
pi-thon42 <mbenaragama@nastudents.org>
2025-06-17 06:43:05 -0400

    added node class

--- alpha_beta_search.py
+++ alpha_beta_search.py
@@ -1,3 +1,10 @@
+class Node:
+    def __init__(self, name, children=None, value=None):
+        self.name = name
+        self.children = children if children is not None else []
+        self.value = value
 
-def main(alpha,beta):
-    
+    def evaluate(node):
+        return node.value
+    def is_terminal(node):
+        return node.value is not None
