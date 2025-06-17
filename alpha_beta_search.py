fe80b2e2f0a9efbcd46e9a1de5d29b2955b13cfa
pi-thon42 <mbenaragama@nastudents.org>
2025-06-17 13:56:06 -0400

    refactored

--- alpha_beta_search.py
+++ alpha_beta_search.py
@@ -13,19 +13,19 @@ def game_over(node):
     pass
 
 # default is that you are the maximizing player
-def alpha_beta(depth, node, maximizing_player=True):
+def minimax(depth, node, maximizing_player=True):
     if depth == 0 or game_over(node):
         return evaluate(node)
     if maximizing_player:
         max_eval = float('-inf')
         for child in node.children:
-            eval = alpha_beta(depth-1,child, False)
+            eval = minimax(depth-1,child, False)
             max_eval = max(max_eval, eval)
         return max_eval
     else:
         min_eval = float('inf')
         for child in node.children:
-            eval = alpha_beta(depth-1, child, True)
+            eval = minimax(depth-1, child, True)
             min_eval = min(min_eval, eval)  
         return min_eval
 
