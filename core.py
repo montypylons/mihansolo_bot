e52344a5ef6f3605aad1f65238900de1ff318944
pi-thon42 <mbenaragama@nastudents.org>
2025-06-19 21:41:46 -0400

    added testing program and refactored some stuff in core.py

--- core.py
+++ core.py
@@ -3,182 +3,147 @@ from chess.engine import PlayResult
 import chess.polyglot
 import random
 
-# todo list
 # TODO: add standing pat eval
+# TODO: add logging
 # TODO: MVV_LAA ordering
 # TODO: refactor loops
 # TODO: use quiet instead of quiescent
 # TODO: reduce scope of variables by putting inside else clause
-# TODO: add Negamax instead of minimax
+# TODO: add Negamax instead of minimax to reduce code complexity
 # TODO: add unit tests.
 
-def order_moves(moves: list) -> list:  # makes alpha/beta search more effective
+
+def order_moves(moves: list) -> list:
     pass
 
 
-def is_quiescent(board: chess.Board) -> bool:
+def is_quiet(board: chess.Board) -> bool:
 
     if any(board.is_capture(move) for move in board.legal_moves):
         return False
-    # if any(board.gives_check(move) for move in board.legal_moves):
-    # return False
 
     return True
 
 
-def quiescence_search(
-    board: chess.Board, alpha: float, beta: float, qdepth=8
+def quiescence_search(  # not using right now, fixing core functions first
+    board: chess.Board, alpha: float, beta: float, qdepth: int = 8
+) -> (
+    int
 ):  # TODO: need to add standing pat evaluation in order to stop the search from going on the the current 1 mil+ positions on the first move.
     print(f"QS depth: {qdepth}, fen: {board.fen()}")
     best_move = None
     non_quiescent = False
-    if is_quiescent(board) or board.is_game_over() or qdepth == 0:
+    if is_quiet(board) or board.is_game_over() or qdepth == 0:
         return evaluate(board), best_move
     else:
-        max_eval = float("-inf")
+        best_eval = float("-inf")
         for move in board.legal_moves:
             if board.is_capture(move):  # or board.gives_check(move):
                 non_quiescent = True
                 board.push(move)
                 eval, _ = quiescence_search(board, -beta, -alpha, qdepth - 1)
-                eval = -eval  # Negamax algo, same as minimax just simpler to implement
+                eval = -eval  # Negamax algo, same as negamax(impler to implement
                 board.pop()
-                if eval > max_eval:
-                    max_eval = eval
+                if eval > best_eval:
+                    best_eval = eval
                     best_move = move
                 alpha = max(alpha, eval)
                 if beta <= alpha:
                     break
         if not non_quiescent:
             return evaluate(board), None
-        return max_eval, best_move
+        return best_eval, best_move
 
 
 # chess functions
-def book_move(board: chess.Board) -> chess.Move | None:
+def find_book_move(board: chess.Board) -> chess.Move | None:
     try:
         with chess.polyglot.open_reader("gm2600.bin") as reader:
-            if reader.find_all(board):
-                return random.choice(list(reader.find_all(board))).move
+            result = reader.find(board)
+            if result:
+                return random.choice(list(result)).move
     except Exception as e:
         return None  # did not work, book doesn't have move for that position
 
 
-def evaluate(board):  
+def evaluate(board: chess.Board) -> int:
     value = 0
-    White = True
+
     if board.is_checkmate():
         if board.turn:
-            return -999999
+            return -999999  # white gets mated
         else:
-            return 999999
+            return 999999  # black gets mated
 
     if (
         board.is_stalemate()
         or board.is_insufficient_material()
         or board.is_repetition()
     ):
-        return 0
+        return 0  # draw
 
-    if White == True:
-        value = value + len(board.pieces(chess.PAWN, chess.WHITE))
-        value = value + len(board.pieces(chess.KNIGHT, chess.WHITE)) * 3
-        value = value + len(board.pieces(chess.BISHOP, chess.WHITE)) * 3
-        value = value + len(board.pieces(chess.ROOK, chess.WHITE)) * 5
-        value = value + len(board.pieces(chess.QUEEN, chess.WHITE)) * 9
+    value = value + len(board.pieces(chess.PAWN, chess.WHITE))
+    value = value + len(board.pieces(chess.KNIGHT, chess.WHITE)) * 3
+    value = value + len(board.pieces(chess.BISHOP, chess.WHITE)) * 3
+    value = value + len(board.pieces(chess.ROOK, chess.WHITE)) * 5
+    value = value + len(board.pieces(chess.QUEEN, chess.WHITE)) * 9
 
-        value = value - len(board.pieces(chess.PAWN, chess.BLACK))
-        value = value - len(board.pieces(chess.KNIGHT, chess.BLACK)) * 3
-        value = value - len(board.pieces(chess.BISHOP, chess.BLACK)) * 3
-        value = value - len(board.pieces(chess.ROOK, chess.BLACK)) * 5
-        value = value - len(board.pieces(chess.QUEEN, chess.BLACK)) * 9
-        return value
-    """else:
-        value = value - len(board.pieces(chess.PAWN, chess.WHITE))
-        value = value - len(board.pieces(chess.KNIGHT, chess.WHITE)) * 3
-        value = value - len(board.pieces(chess.BISHOP, chess.WHITE)) * 3
-        value = value - len(board.pieces(chess.ROOK, chess.WHITE)) * 5
-        value = value - len(board.pieces(chess.QUEEN, chess.WHITE)) * 9
-
-        value = value + len(board.pieces(chess.PAWN, chess.BLACK))
-        value = value + len(board.pieces(chess.KNIGHT, chess.BLACK)) * 3
-        value = value + len(board.pieces(chess.BISHOP, chess.BLACK)) * 3
-        value = value + len(board.pieces(chess.ROOK, chess.BLACK)) * 5
-        value = value + len(board.pieces(chess.QUEEN, chess.BLACK)) * 9
-
-    return value"""
+    value = value - len(board.pieces(chess.PAWN, chess.BLACK))
+    value = value - len(board.pieces(chess.KNIGHT, chess.BLACK)) * 3
+    value = value - len(board.pieces(chess.BISHOP, chess.BLACK)) * 3
+    value = value - len(board.pieces(chess.ROOK, chess.BLACK)) * 5
+    value = value - len(board.pieces(chess.QUEEN, chess.BLACK)) * 9
+    # These show the material imbalance of how many more points of white material there is
+    return (
+        value if board.turn else -value
+    )  # should always return from the POV of side to move
 
 
-def search(board):  # TODO: add quiescence search and iterative deepening
-    _book_move = book_move(board)
-    if _book_move:
-        print(f"Book move: {_book_move}")
-        return PlayResult(_book_move, None)
+def search(board: chess.Board):  # TODO: add quiescence search and iterative deepening
+    book_move = find_book_move(board)
+    if book_move:
+        print(f"Book move: {book_move}")
+        return PlayResult(book_move, None)
     depth = 5
-    _, best_move = minimax(float("-inf"), float("inf"), None, depth, board, board.turn)
+
+    _, best_move = negamax("-inf", float("inf"), None, depth, board, board.turn)
 
     if best_move:
         return PlayResult(best_move, None)
-        # return best_move
     else:
-        raise TypeError("Minimax returned a None value, which is bad.")
+        raise TypeError("negamax returned a None value, something went wrong")
 
 
-# minimax algos
-def game_over(board):  # TODO: implement this
-    # check for checkmate, stalemate, or insufficient material
+def game_over(board: chess.Board) -> bool:
     if board.is_checkmate() or board.is_stalemate() or board.is_insufficient_material():
         return True
-    return False  # placeholder, always returns False
+    return False
 
 
-# default is that you are the maximizing player
-def minimax(alpha, beta, last_move, depth, board, maximizing_player=True):
-    best_move = None
+def negamax(
+    alpha: float, beta: float, last_move: chess.Move, depth: int, board: chess.Board
+) -> tuple[float, chess.Move | None]:
+
     if depth == 0 or game_over(board):
-        return quiescence_search(board, alpha, beta)[0], last_move
-    if maximizing_player:
-        max_eval = float("-inf")
-        for move in board.legal_moves:
-            board.push(move)
-            eval, _ = minimax(alpha, beta, move, depth - 1, board, False)
-            board.pop()
-            if eval > max_eval:
-                max_eval = eval
-                best_move = move
+        # return quiescence_search(board, alpha, beta)[0], last_move
+        return evaluate(board), last_move
 
-            alpha = max(alpha, eval)
-            if beta <= alpha:
-                break
-        return max_eval, best_move
+    best_move = None
+    best_eval = float("-inf")
 
-    else:
-        min_eval = float("inf")
-        for move in board.legal_moves:
-            board.push(move)
-
-            eval, _ = minimax(alpha, beta, move, depth - 1, board, True)
-            board.pop()
-            if eval < min_eval:
-                min_eval = eval
-                best_move = move
-            beta = min(beta, eval)
-
-            if beta <= alpha:
-                break
-        return min_eval, best_move
+    for move in board.legal_moves:
+        board.push(move)
+        score, _ = negamax(-beta, -alpha, move, depth - 1, board)
+        score = -score
+        board.pop()
+        if score > best_eval:
+            best_eval = score
+            best_move = move
+        alpha = max(alpha, score)
+        if beta <= alpha:
+            break
+    return best_eval, best_move
 
 
-def test():
-    board = chess.Board()
-    for i in range(10):
-        result = search(board)
-        if result:
-            board.push(result.move)
-            print(f"Best move: {result.move}", "move number", i + 1)
-        print(board)
-
-
-test()
-# TODO: learn basic chess heuristics
-# TODO: add NNUE eval function
+def parse_uci():
+    pass  # add UCI support for SPRT testing later
---
+++ testing.py
@@ -0,0 +1,16 @@
+from core import search
+import chess
+
+
+def main():
+    board = chess.Board()
+    print("initializing match of engine vs engine, thanks for watching!")
+    while not board.is_game_over():
+        print("Current state of board: ", board)
+        white_move= search(board)
+        print("White move: ", white_move)
+        board.push(white_move.move)
+        black_move = search(board)
+        print("Black move: ", black_move)
+        board.push(black_move.move)
+    print("Game result: ", board.result())
