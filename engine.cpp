0be2910e82ae7fa5373535ec3085826a0f085eae
pi-thon42 <mbenaragama@nastudents.org>
2025-06-23 13:23:16 -0400

    added stuff for CMake

--- engine.cpp
+++ engine.cpp
@@ -1,1 +1,13 @@
 #include "chess.hpp"
+#include <tuple>
+#include <string>
+
+using namespace chess;
+
+std::tuple<int, std::string, bool> evaluate(chess::Board board) {
+    chess::Position chess_position = chess::Position();
+    std::string name = "Alice";
+    bool active = true;
+    return std::make_tuple(id, name, active);
+}
+
