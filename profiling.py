1048823a9a8a4dca74005754c3ed2f9f696b7869
pi-thon42 <mbenaragama@nastudents.org>
2025-06-23 21:36:02 -0400

    65% speed improvement on evaluation,

--- profiling.py
+++ profiling.py
@@ -6,3 +6,6 @@ import chess
 board = chess.Board("8/8/B4R2/7R/3pk3/P7/P1PPPP1P/2BQK3 w - - 11 37")
 
 cProfile.run('core2.search(board=board)')
+print("="*100)
+print("core VERSION 1")
+cProfile.run('core.search(board=board)')
