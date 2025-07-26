20d068c8185fee0148baa6ec5bc4a9d1a73e71f9
pi-thon42 <mbenaragama@nastudents.org>
2025-07-26 16:31:07 -0400

    starting work on TT

---
+++ src/tt.cpp
@@ -0,0 +1,25 @@
+#include <cstdint>
+#include "chess.hpp"
+#include "tt.hpp"
+
+enum class NodeType: uint8_t
+{
+    EXACT,
+    UPPERBOUND,
+    LOWERBOUND
+};
+
+struct TTEntry // TODO: implement transposition table
+{
+    uint64_t zobrist_key;
+    chess::Move best_move;
+    int depth;
+    int score;
+    NodeType node_type;
+    int age;
+};
+
+class TranspositionTable
+{
+    int address_calc(uint64_t key);
+};
