537bdb930165c61e4716b708769dbb824bb75324
pi-thon42 <mbenaragama@nastudents.org>
2025-07-30 15:49:38 -0400

    timemanagement integrated

--- src/timemanagement.cpp
+++ src/timemanagement.cpp
@@ -1,22 +1,24 @@
 #include "timemanagement.hpp"
 #include <chrono>
 
-
-bool TimeManager::time_remaining() const
+namespace TimeManagement
 {
-    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - last_move_time).
-        count() > time_remaining_for_move;
-}
-
-void TimeManager::go(const int wtime, const int btime, const int winc, const int binc)
-{
-    last_move_time = std::chrono::steady_clock::now();
-    if (white)
+    bool TimeManager::time_remaining() const
     {
-        time_remaining_for_move = wtime / 20 + winc / 2;
+        return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - last_move_time).
+            count() > time_remaining_for_move;
     }
-    if (!white)
+
+    void TimeManager::go(const int wtime, const int btime, const int winc, const int binc)
     {
-        time_remaining_for_move = btime / 20 + binc / 2;
+        last_move_time = std::chrono::steady_clock::now();
+        if (white)
+        {
+            time_remaining_for_move = wtime / 20 + winc / 2;
+        }
+        if (!white)
+        {
+            time_remaining_for_move = btime / 20 + binc / 2;
+        }
     }
 }
