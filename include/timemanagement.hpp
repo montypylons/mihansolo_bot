f3dc33c2068c3960f72dcbbd89702937c92f8538
pi-thon42 <mbenaragama@nastudents.org>
2025-07-30 14:20:10 -0400

    working on time management

--- include/timemanagement.hpp
+++ include/timemanagement.hpp
@@ -1,2 +1,18 @@
 #pragma once
+#include <chrono>
 
+class TimeManager
+{
+public:
+    explicit TimeManager(const bool side_to_move) : white(side_to_move)
+    {
+    }
+
+    [[nodiscard]] bool time_remaining() const; // have you outrun the time allowed for this move
+    void go(int wtime, int btime, int winc, int binc);
+
+private:
+    bool white; // true if bot is White, false if bot is Black
+    int time_remaining_for_move = 0;
+    std::chrono::steady_clock::time_point last_move_time;
+};
