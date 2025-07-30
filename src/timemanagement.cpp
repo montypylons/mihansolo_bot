5a247de0d684fad1e799e309fe0cfd39cf7d3663
pi-thon42 <mbenaragama@nastudents.org>
2025-07-30 14:20:10 -0400

    working on time management

--- src/timemanagement.cpp
+++ src/timemanagement.cpp
@@ -1,2 +1,22 @@
 #include "timemanagement.hpp"
+#include <chrono>
 
+
+bool TimeManager::time_remaining() const
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - last_move_time).
+        count() > time_remaining_for_move;
+}
+
+void TimeManager::go(const int wtime, const int btime, const int winc, const int binc)
+{
+    last_move_time = std::chrono::steady_clock::now();
+    if (white)
+    {
+        time_remaining_for_move = wtime / 20 + winc / 2;
+    }
+    if (!white)
+    {
+        time_remaining_for_move = btime / 20 + binc / 2;
+    }
+}
