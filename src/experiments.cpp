da5af1b0755c471866674e8c9666152f20d956ed
pi-thon42 <mbenaragama@nastudents.org>
2025-09-03 20:26:49 -0400

    run sm more trials

--- src/experiments.cpp
+++ src/experiments.cpp
@@ -236,7 +236,7 @@ bool MRE()
 
     constexpr auto commands =
         "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3 e8g8 b2b3 c6b4 c2c3\n"
-        "go wtime 60000 btime 60000\n" //wtime 66130 btime 42879 winc 1000 binc 1000
+        "go depth 9\n" //wtime 66130 btime 42879 winc 1000 binc 1000
         "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3 e8g8 b2b3 c6b4 c2c3 b4c2 e1e2\n"
         "go depth 2";
 
