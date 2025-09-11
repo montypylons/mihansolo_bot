8e3fe21a523c49488e409ce3e2a00115ae2b566d
pi-thon42 <mbenaragama@nastudents.org>
2025-09-11 18:00:15 -0400

    ahhhhhhhhhhhhhhhhhhhhhhhhh sugar snaps

--- src/experiments.cpp
+++ src/experiments.cpp
@@ -12,6 +12,32 @@ constexpr auto current_M_success_commands_ik_my_variable_names_are_bad =
     "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3 e8g8 b2b3 c6b4 c2c3 b4c2 e1e2\n"
     "go depth 2";
 
+constexpr auto commands_actually_original =
+    "ucinewgame\n"
+    "isready\n"
+    "position startpos moves e2e3\n"
+    "go movetime 10000\n"
+    "setoption name Ponder value true\n"
+    "position startpos moves e2e3 b8c6 b1c3\n"
+    "go wtime 60470 btime 57999 winc 1000 binc 1000\n"
+    "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4\n"
+    "go wtime 61290 btime 55489 winc 1000 binc 1000\n"
+    "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4\n"
+    "go wtime 62100 btime 53109 winc 1000 binc 1000\n"
+    "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4\n"
+    "go wtime 62910 btime 50849 winc 1000 binc 1000\n"
+    "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1\n"
+    "go wtime 63710 btime 48699 winc 1000 binc 1000\n"
+    "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3\n"
+    "go wtime 64520 btime 46659 winc 1000 binc 1000\n"
+    "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3 e8g8 b2b3\n"
+    "go wtime 65330 btime 44719 winc 1000 binc 1000\n"
+    "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3 e8g8 b2b3 c6b4 c2c3\n"
+    "go wtime 66130 btime 42879 winc 1000 binc 1000\n"
+    "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3 e8g8 b2b3 c6b4 c2c3 b4c2 e1e2\n"
+    "go wtime 66950 btime 41129 winc 1000 binc 1000";
+
+
 /**
  * 100 middlegame puzzle FENs scraped from https://database.lichess.org
  */
@@ -234,30 +260,6 @@ bool MRE()
 {
     // this is known to fail, typically under 25/50 trials succeed
     constexpr auto target_move = "bestmove d4d3";
-    constexpr auto commands_actually_original =
-        "ucinewgame\n"
-        "isready\n"
-        "position startpos moves e2e3\n"
-        "go movetime 10000\n"
-        "setoption name Ponder value true\n"
-        "position startpos moves e2e3 b8c6 b1c3\n"
-        "go wtime 60470 btime 57999 winc 1000 binc 1000\n"
-        "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4\n"
-        "go wtime 61290 btime 55489 winc 1000 binc 1000\n"
-        "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4\n"
-        "go wtime 62100 btime 53109 winc 1000 binc 1000\n"
-        "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4\n"
-        "go wtime 62910 btime 50849 winc 1000 binc 1000\n"
-        "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1\n"
-        "go wtime 63710 btime 48699 winc 1000 binc 1000\n"
-        "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3\n"
-        "go wtime 64520 btime 46659 winc 1000 binc 1000\n"
-        "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3 e8g8 b2b3\n"
-        "go wtime 65330 btime 44719 winc 1000 binc 1000\n"
-        "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3 e8g8 b2b3 c6b4 c2c3\n"
-        "go wtime 66130 btime 42879 winc 1000 binc 1000\n"
-        "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3 e8g8 b2b3 c6b4 c2c3 b4c2 e1e2\n"
-        "go wtime 66950 btime 41129 winc 1000 binc 1000";
     constexpr auto commands_original =
         "position startpos moves e2e3 b8c6 b1c3 e7e5 d1g4 g8f6 g4c4 d7d5 c4a4 d5d4 c3d1 f8c5 g1f3 e8g8 b2b3 c6b4 c2c3\n"
         "go wtime 66130 btime 42879 winc 1000 binc 1000\n"
@@ -307,31 +309,40 @@ bool negamax_MRE(TranspositionTable& table)
     return false;
 }
 
+void RAW()
+{
+    std::istringstream input(commands_actually_original);
+    engine::start_uci(input);
+}
+
 int main()
 {
-    std::cout << "started main\n\n";
-    std::cout << "Minimal reproducible example\n\n";
-    // std::cout << "Experiments\n\n";
-    std::cout << "ITERATIONS: " << current_MRE_iterations << std::endl;
-    int SUCCESS = 0;
-    int FAILURE = 0;
-    for (int i = 0; i < current_MRE_iterations; i++)
-    {
-        // if (TranspositionTable table; negamax_MRE(table))
-        if (MRE())
+    /*
+        std::cout << "started main\n\n";
+        std::cout << "Minimal reproducible example\n\n";
+        // std::cout << "Experiments\n\n";
+        std::cout << "ITERATIONS: " << current_MRE_iterations << std::endl;
+        int SUCCESS = 0;
+        int FAILURE = 0;
+        for (int i = 0; i < current_MRE_iterations; i++)
         {
-            SUCCESS++;
-            std::cout << "SUCCESS\n";
+            // if (TranspositionTable table; negamax_MRE(table))
+            if (MRE())
+            {
+                SUCCESS++;
+                std::cout << "SUCCESS\n";
+            }
+            else
+            {
+                FAILURE++;
+                std::cout << "FAILURE\n";
+            }
         }
-        else
-        {
-            FAILURE++;
-            std::cout << "FAILURE\n";
-        }
-    }
 
-    std::cout << "Success rate (found d4d3): " << SUCCESS << "/" << SUCCESS + FAILURE << std::endl;
-    return 0;
-    getPrimes();
+        std::cout << "Success rate (found d4d3): " << SUCCESS << "/" << SUCCESS + FAILURE << std::endl;
+        return 0;
+        getPrimes();
+        */
+    RAW();
     return 0;
 }
