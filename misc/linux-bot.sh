b013662ff62515301c7626103797f61f46f77763
Mihin Benaragama <182445227+montypylons@users.noreply.github.com>
2025-09-17 14:05:50 +0000

    f

--- misc/linux-bot.sh
+++ misc/linux-bot.sh
@@ -4,4 +4,5 @@ current_date=$(date +"%m_%d_%Y")
 
 echo "Starting lichess-bot"
 
-python ../lichess-bot/lichess-bot.py --config ../config/linux-config.yml > "${current_date}.log"
+cd ../lichess-bot
+python lichess-bot.py --config ../config/linux-config.yml > "${current_date}.log"
