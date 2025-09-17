86dd5d553c37aeaa021a3945b29d36076eb4a8b4
pi-thon42 <mbenaragama@nastudents.org>
2025-09-17 15:58:32 -0400

    made america great again

--- misc/windows-bot.ps1
+++ misc/windows-bot.ps1
@@ -5,4 +5,4 @@ $currentDate = Get-Date -Format "M_d_yyyy"
 # Define log file name
 $logFile = "$currentDate.log"
 cd ../lichess-bot
-python lichess-bot.py -v --config.yml ../config/windows-config.yml | Out-File -FilePath ../logs/windows/$logFile
+python lichess-bot.py -v --config.yml ../config/windows-accept-bot.yml | Out-File -FilePath ../logs/windows/$logFile
