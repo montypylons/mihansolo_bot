ea4b3e8865db252cf11829f8853f516e6b6c934a
Mihin Benaragama <182445227+montypylons@users.noreply.github.com>
2025-09-18 20:11:28 +0000

    add concurrency to cutechess script

--- misc/cutechess_cmd.sh
+++ misc/cutechess_cmd.sh
@@ -5,7 +5,8 @@ date_time=$(date +"%Y-%m-%d_%H:%M:%S")
 cutechess-cli \
   -engine cmd="../build/engine" name=mihan1 \
   -engine cmd="../build/engine" name=mihan2 \
+  -concurrency 3 \
   -each proto=uci tc=1+1 \
-  -rounds 11 -games 2 \
+  -rounds 20 -games 2 \
   -pgnout "../logs/cutechess/tournament_${date_time}.pgn" \
   -debug > "../logs/cutechess/debug_${date_time}.log"
