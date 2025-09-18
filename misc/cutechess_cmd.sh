9dec5031572534edb2efe516c8a635c531ae6c43
Mihin Benaragama <182445227+montypylons@users.noreply.github.com>
2025-09-18 20:08:17 +0000

    ... why is ts getting commited

--- misc/cutechess_cmd.sh
+++ misc/cutechess_cmd.sh
@@ -5,7 +5,7 @@ date_time=$(date +"%Y-%m-%d_%H:%M:%S")
 cutechess-cli \
   -engine cmd="../build/engine" name=mihan1 \
   -engine cmd="../build/engine" name=mihan2 \
-  -each proto=uci tc=1+2 \
-  -rounds 10 -games 2 \
+  -each proto=uci tc=1+1 \
+  -rounds 11 -games 2 \
   -pgnout "../logs/cutechess/tournament_${date_time}.pgn" \
   -debug > "../logs/cutechess/debug_${date_time}.log"
