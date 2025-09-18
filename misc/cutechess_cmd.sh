36e29ab283253c0e00f17a5f8228bff546ce8226
Mihin Benaragama <182445227+montypylons@users.noreply.github.com>
2025-09-18 20:00:58 +0000

    fixed up the script

--- misc/cutechess_cmd.sh
+++ misc/cutechess_cmd.sh
@@ -1,7 +1,11 @@
-cutechess-cli.exe `
-  -engine cmd="C:\Users\DELL\Documents\mihansolo_bot\cmake-build-release\engine.exe" name=mihan `
-  -engine cmd="C:\Users\DELL\Documents\mihansolo_bot\cmake-build-release\engine.exe" name=Komodo `
-  -each proto=uci tc=1+2 `
-  -rounds 10 -games 2 `
-  -pgnout tourney_9_18_2025.pgn `
-  -debug
+#!/bin/bash
+
+date_time = $(date +"%Y-%m-%d_%H:%M:%S")
+
+cutechess-cli \
+  -engine cmd="../build/engine" name=mihan1 \
+  -engine cmd="../build/engine" name=mihan2 \
+  -each proto=uci tc=1+2 \
+  -rounds 10 -games 2 \
+  -pgnout ../logs/cutechess/tournament_$(date_time).pgn \
+  -debug > ../logs/cutechess/debug_$(date_time).log
