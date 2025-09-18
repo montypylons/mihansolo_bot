5ef956220fa8af6e760a730b1cdf4dd73f16f009
Mihin Benaragama <182445227+montypylons@users.noreply.github.com>
2025-09-18 20:14:41 +0000

    concurrency will screw up logs

--- misc/cutechess_cmd.sh
+++ misc/cutechess_cmd.sh
@@ -5,7 +5,6 @@ date_time=$(date +"%Y-%m-%d_%H:%M:%S")
 cutechess-cli \
   -engine cmd="../build/engine" name=mihan1 \
   -engine cmd="../build/engine" name=mihan2 \
-  -concurrency 3 \
   -each proto=uci tc=1+1 \
   -rounds 20 -games 2 \
   -pgnout "../logs/cutechess/tournament_${date_time}.pgn" \
