c064da3cbd9f8de8172a51243085619b47306642
Mihin Benaragama <182445227+montypylons@users.noreply.github.com>
2025-09-18 20:04:30 +0000

    les go

--- misc/cutechess_cmd.sh
+++ misc/cutechess_cmd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-date_time = $(date +"%Y-%m-%d_%H:%M:%S")
+date_time=$(date +"%Y-%m-%d_%H:%M:%S")
 
 cutechess-cli \
   -engine cmd="../build/engine" name=mihan1 \
   -engine cmd="../build/engine" name=mihan2 \
   -each proto=uci tc=1+2 \
   -rounds 10 -games 2 \
-  -pgnout ../logs/cutechess/tournament_$(date_time).pgn \
-  -debug > ../logs/cutechess/debug_$(date_time).log
+  -pgnout "../logs/cutechess/tournament_${date_time}.pgn" \
+  -debug > "../logs/cutechess/debug_${date_time}.log"
