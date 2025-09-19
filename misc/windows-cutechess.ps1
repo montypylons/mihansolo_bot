d0617ae433b14c95bbc20b89c71f76f993cc416e
pi-thon42 <mbenaragama@nastudents.org>
2025-09-19 16:23:39 -0400

    formatting

--- misc/windows-cutechess.ps1
+++ misc/windows-cutechess.ps1
@@ -1,3 +1,2 @@
-
 $currentDate = Get-Date -Format "yyyy-MM-dd_HH:mm:ss"
 cutechess-cli -engine cmd="../cmake-build-release/engine.exe" name=mihan1 -engine cmd="../cmake-build-release/engine.exe" name=mihan2 -each proto=uci tc=200/60 -rounds 20 -games 2 -pgnout "../logs/cutechess/tournament_${currentDate}.pgn" -debug | Tee-Object -FilePath "../logs/cutechess/debug_${currentDate}.log" -Append
