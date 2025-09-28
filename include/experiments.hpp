1665927e0e67374bd4015dd5da6cf844620f2423
pi-thon42 <mbenaragama@nastudents.org>
2025-09-28 11:15:08 -0400

    Revert "clean"
    This reverts commit a40c086043e4e8cc9f97aa24bad4c6e1e215b844.

--- include/experiments.hpp
+++ include/experiments.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include "chess.hpp"
 
 constexpr auto target_fen = "rnbqk2r/ppp1p1bp/3p1np1/5p2/2PP4/5NP1/PP2PPBP/RNBQ1RK1 b kq - 1 6";
 constexpr auto target_move = chess::Move::make<chess::Move::NORMAL>(chess::Square::SQ_F5, chess::Square::SQ_C2);
